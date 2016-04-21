618bda2c310683cc5f39ce9ccde2a340748e9583
jonas-cromheeke <jonas_cromheeke@msn.com>
2016-04-21 14:57:09 +0200

    update packages

---
+++ JavaHelloWorld/src/test/java/HelloWorldTest.java
@@ -0,0 +1,7 @@
+package test.java;
+
+/**
+ * Created by cromhjo on 21/04/2016.
+ */
+public class HelloWorldTest {
+}
--- src/main/java/test.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by cromhjo on 21/04/2016.
- */
-public class test {
-}
