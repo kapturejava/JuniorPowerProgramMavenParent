280652faf15f2aa11e8356ff4d1f1b3524ad51b0
nilsdhont <nilsdhont@gmail.com>
2016-04-22 11:39:34 +0200

    format

--- JavaHelloWorld/src/main/java/be/kapture/HelloWorld.java
+++ JavaHelloWorld/src/main/java/be/kapture/HelloWorld.java
@@ -1,10 +1,11 @@
-package main.java.be.kapture;
+package be.kapture;
 
 /**
  * Created by dhondn1 on 21/04/2016.
  */
 public class HelloWorld {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
         System.out.print("Hello World");
     }
 }
