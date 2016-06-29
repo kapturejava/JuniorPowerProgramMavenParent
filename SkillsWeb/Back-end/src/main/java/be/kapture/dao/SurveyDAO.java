c36b18a7558ab6e132b76763b321618a728002b0
Yannick Thibos <yannick.thibos@e2partners.be>
2016-06-29 16:15:52 +0200

    Added method findByPersonId

--- SkillsWeb/Back-end/src/main/java/be/kapture/dao/SurveyDAO.java
+++ SkillsWeb/Back-end/src/main/java/be/kapture/dao/SurveyDAO.java
@@ -1,5 +1,6 @@
 package be.kapture.dao;
 
+import java.util.Collection;
 import java.util.List;
 
 import be.kapture.entities.Survey;
@@ -19,4 +20,7 @@ public class SurveyDAO extends AbstractDAO<Survey> {
 		return (List<Survey>) getCurrentSession().createQuery("select s from Survey s").list();
 	}
 
+	public List<Survey> findByPersonId(int id) {
+		return (List<Survey>) getCurrentSession().createQuery("select s from Survey s where s.person.id = s.id");
+	}
 }
