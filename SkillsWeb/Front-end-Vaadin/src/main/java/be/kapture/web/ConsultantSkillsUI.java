788cc116525d7146d68007bed708d0592339a80b
Yannick Thibos <yannick.thibos@e2partners.be>
2016-06-29 16:16:45 +0200

    Tried to add another ComboBox to display Surveys, introduced errors.
    NPE upon retrieving session from HibernateUtil.getSession.

--- SkillsWeb/Front-end-Vaadin/src/main/java/be/kapture/web/ConsultantSkillsUI.java
+++ SkillsWeb/Front-end-Vaadin/src/main/java/be/kapture/web/ConsultantSkillsUI.java
@@ -1,11 +1,16 @@
 package be.kapture.web;
 
 import be.kapture.dao.PersonDAO;
+import be.kapture.dao.SurveyDAO;
 import be.kapture.entities.Person;
+import be.kapture.entities.Survey;
 import be.kapture.util.SkillNotFoundException;
 import be.kapture.web.forms.ConsultantSkillsRetrieverForm;
 import be.kapture.web.forms.SkillsetForm;
 import com.vaadin.annotations.Title;
+import com.vaadin.data.Property;
+import com.vaadin.data.Property.ValueChangeEvent;
+import com.vaadin.data.Property.ValueChangeListener;
 import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.*;
@@ -22,14 +27,20 @@ import java.util.List;
 public class ConsultantSkillsUI extends UI {
 
     private final HorizontalSplitPanel horizontalSplitPanel = new HorizontalSplitPanel();
+    private final VerticalLayout leftPanel = new VerticalLayout();
 
     private final transient PersonDAO personDAO = new PersonDAO();
+    private final transient SurveyDAO surveyDAO = new SurveyDAO();
 
     @Override
     protected void init(VaadinRequest request) {
 
         horizontalSplitPanel.setSplitPosition(33F, Unit.PERCENTAGE);
         horizontalSplitPanel.setLocked(true);
+
+        leftPanel.setMargin(true);
+        leftPanel.setSpacing(true);
+        horizontalSplitPanel.setFirstComponent(leftPanel);
 
         setupLeftPanel();
 
@@ -38,14 +49,9 @@ public class ConsultantSkillsUI extends UI {
 
     private void setupLeftPanel() {
 
-        HorizontalLayout leftPanel = new HorizontalLayout();
-        leftPanel.setMargin(true);
-        leftPanel.setSpacing(true);
-
         ComboBox consultantsComboBox = createConsultantsComboBox();
 
         leftPanel.addComponent(consultantsComboBox);
-        horizontalSplitPanel.addComponent(leftPanel);
 
     }
 
@@ -55,11 +61,40 @@ public class ConsultantSkillsUI extends UI {
 
         BeanItemContainer<Person> consultantContainer = new BeanItemContainer<>(Person.class);
         consultantContainer.addAll(personDAO.findAll());
-
         comboBox.setContainerDataSource(consultantContainer);
+
+        comboBox.addValueChangeListener(new ConsultantSelectedListener());
 
         return comboBox;
 
     }
 
+    private class ConsultantSelectedListener implements ValueChangeListener {
+
+        @Override
+        public void valueChange(ValueChangeEvent event) {
+            if (event.getProperty().getValue() != null) {
+                Person selectedConsultant = (Person) event.getProperty().getValue();
+
+                System.out.println("------------------SELECTED CONSULTANT-----------------\n" + selectedConsultant
+                        + " WITH ID = " + selectedConsultant.getId() + "\n");
+
+                leftPanel.addComponent(createSurveysComboBox(selectedConsultant.getId()));
+            }
+        }
+    }
+
+    private ComboBox createSurveysComboBox(int consultantID) {
+
+        ComboBox comboBox = new ComboBox("Surveys:");
+
+        BeanItemContainer<Survey> surveyContainer = new BeanItemContainer<>(Survey.class);
+        List<Survey> foundSurveys = surveyDAO.findByPersonId(consultantID);
+        System.out.println("------------------FOUND SURVEYS-----------------\n" + foundSurveys
+                + "\n");
+        surveyContainer.addAll(foundSurveys);
+        comboBox.setContainerDataSource(surveyContainer);
+
+        return comboBox;
+    }
 }
