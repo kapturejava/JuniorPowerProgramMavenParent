5f21f7b4543d9326b0d1a62b24657422c4970bdf
Dario Jeurissen <d.jeurissen@gmail.com>
2017-01-20 15:20:09 +0100

    Jo had weer commentaar

--- TailorFitParent/TailorFit-Back-end/src/main/java/be/kapture/repositories/ClothingItemRepository.java
+++ TailorFitParent/TailorFit-Back-end/src/main/java/be/kapture/repositories/ClothingItemRepository.java
@@ -7,29 +7,4 @@ public class ClothingItemRepository extends AbstractRepository<ClothingItem>{
 	public ClothingItemRepository(Class<ClothingItem> typeClass) {
 		super(typeClass);
 	}
-
-//	@Override
-//	public void create(ClothingItem t) {
-//		// TODO Auto-generated method stub
-//		super.create(t);
-//	}
-//	
-//	@Override
-//	public ClothingItem read(int id) {
-//		// TODO Auto-generated method stub
-//		return super.read(id);
-//	}
-//	
-//	@Override
-//	public void update(ClothingItem t) {
-//		// TODO Auto-generated method stub
-//		super.update(t);
-//	}
-//	
-//	@Override
-//	public void delete(ClothingItem t) {
-//		// TODO Auto-generated method stub
-//		super.delete(t);
-//	}
-	
 }
