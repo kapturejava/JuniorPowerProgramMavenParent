591361e14180668b8c83ce5ffc4d405aefb57db1
Unknown <jo.van.montfort1@telenet.be>
2017-01-20 10:32:33 +0100

    Added Hibernate Config / check person class

--- TailorFitParent/TailorFit-Back-end/src/main/db/TailorFit.sql
+++ TailorFitParent/TailorFit-Back-end/src/main/db/TailorFit.sql
@@ -15,7 +15,8 @@ DROP TABLE IF EXISTS Person;
 CREATE TABLE Person (
 	  id int(11) NOT NULL auto_increment,
       Size int(11) NOT NULL,
-      Gender varchar(50) NOT NULL DEFAULT 'male',
-      Name varchar(50) NOT NULL DEFAULT '',
+       Gender int(11) NOT NULL DEFAULT 0,
+      FirstName varchar(50) NOT NULL DEFAULT '',
+      LastName varchar(50) NOT NULL DEFAULT '',
       PRIMARY KEY (id)
     ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 ;
--- TailorFitParent/TailorFit-Back-end/src/main/java/be/kapture/entities/Person.java
+++ TailorFitParent/TailorFit-Back-end/src/main/java/be/kapture/entities/Person.java
@@ -4,89 +4,90 @@ import be.kapture.enums.Gender;
 import be.kapture.util.HelperClass;
 
 public class Person {
-	
-	private String firstname;
-	private String lastname;
-	private int size;
-	private Gender gender;
-	
 
-	public Person(String firstname, String lastname, int size, Gender gender) throws IllegalArgumentException{
-		setFirstName(firstname);
-		setLastName(lastname);
-		setSize(size);
-		setGender(gender);
-	}
+    private long id;
+    private String firstName;
+    private String lastName;
+    private int size;
+    private Gender gender;
 
-	public Person() {
-		this.firstname = "default";
-		this.lastname = "default";
-		this.size = 150;
-		this.gender = gender.MALE;
-	}
+    protected Person() {
+        // TODO Auto-generated constructor stub
+    }
 
-	public void setFirstName(String firstname) {
-		this.firstname = HelperClass.isStringEmpty(firstname);
-		
-	}
+    public Person(String firstName, String lastName, int size, Gender gender) throws IllegalArgumentException {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setSize(size);
+        setGender(gender);
+    }
 
-	public void setLastName(String lastname) {
-		this.lastname = HelperClass.isStringEmpty(lastname);
-	}
+    public void setFirstName(String firstname) {
+        this.firstName = HelperClass.isStringEmpty(firstname);
 
-	public long getId() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    }
 
-	public void setSize(int size) {
-		this.size = HelperClass.isIntStricltyPositive(size);
-		
-		
-	}
+    public void setLastName(String lastname) {
+        this.lastName = HelperClass.isStringEmpty(lastname);
+    }
 
-	public void setGender(Gender gender) {
-		this.gender = HelperClass.isEnumNotEmpty(gender);
-		
-	}
+    public void setId(long id) {
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((firstname == null) ? 0 : firstname.hashCode());
-		result = prime * result + ((gender == null) ? 0 : gender.hashCode());
-		result = prime * result + ((lastname == null) ? 0 : lastname.hashCode());
-		result = prime * result + size;
-		return result;
-	}
+        this.id = id;
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Person other = (Person) obj;
-		if (firstname == null) {
-			if (other.firstname != null)
-				return false;
-		} else if (!firstname.equals(other.firstname))
-			return false;
-		if (gender != other.gender)
-			return false;
-		if (lastname == null) {
-			if (other.lastname != null)
-				return false;
-		} else if (!lastname.equals(other.lastname))
-			return false;
-		if (size != other.size)
-			return false;
-		return true;
-	}
-	
-	
+    }
+
+    public long getId() {
+
+        return id;
+
+    }
+
+    public void setSize(int size) {
+        this.size = HelperClass.isIntStricltyPositive(size);
+
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = HelperClass.isEnumNotEmpty(gender);
+
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+        result = prime * result + ((gender == null) ? 0 : gender.hashCode());
+        result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+        result = prime * result + size;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Person other = (Person) obj;
+        if (firstName == null) {
+            if (other.firstName != null)
+                return false;
+        } else if (!firstName.equals(other.firstName))
+            return false;
+        if (gender != other.gender)
+            return false;
+        if (lastName == null) {
+            if (other.lastName != null)
+                return false;
+        } else if (!lastName.equals(other.lastName))
+            return false;
+        if (size != other.size)
+            return false;
+        return true;
+    }
 
 }
