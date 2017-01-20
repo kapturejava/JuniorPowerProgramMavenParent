0ab7b9df1a11324a5eb6fb012945b0ab93d50d2a
Unknown <jo.van.montfort1@telenet.be>
2017-01-20 11:39:46 +0100

    Added default constructor @PersonRepository

--- TailorFitParent/TailorFit-Back-end/src/main/java/be/kapture/repositories/PersonRepository.java
+++ TailorFitParent/TailorFit-Back-end/src/main/java/be/kapture/repositories/PersonRepository.java
@@ -2,26 +2,29 @@ package be.kapture.repositories;
 
 import be.kapture.entities.Person;
 
-public class PersonRepository{
+public class PersonRepository extends AbstractRepository<Person> {
 
-	public void create(Person person) {
-		// TODO Auto-generated method stub
-		
-	}
+    public PersonRepository(Class<Person> typeParameterClass) {
+        super(typeParameterClass);
+    }
 
-	public Object read(long l) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public void create(Person person) {
 
-	public void update(Person person) {
-		// TODO Auto-generated method stub
-		
-	}
+    }
 
-	public void delete(Person person) {
-		// TODO Auto-generated method stub
-		
-	}
+    public Object read(long l) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void update(Person person) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void delete(Person person) {
+        // TODO Auto-generated method stub
+
+    }
 
 }
