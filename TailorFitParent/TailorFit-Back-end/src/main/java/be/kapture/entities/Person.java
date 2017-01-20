da1c8124962cdbaa17d1413cf4ddc452e217fa62
Dario Jeurissen <d.jeurissen@gmail.com>
2017-01-20 09:41:01 +0100

    Revert "Merge remote-tracking branch 'refs/remotes/origin/master'"
    This reverts commit 82e038624444d92e58037c4ad2e1141e59bd3834.

--- TailorFitParent/TailorFit-Back-end/src/main/java/be/kapture/entities/Person.java
+++ TailorFitParent/TailorFit-Back-end/src/main/java/be/kapture/entities/Person.java
@@ -11,63 +11,82 @@ public class Person {
 	private Gender gender;
 	
 
-    private long id;
-    private String firstName;
-    private String lastName;
-    private int size;
-    private Gender gender;
+	public Person(String firstname, String lastname, int size, Gender gender) throws IllegalArgumentException{
+		setFirstName(firstname);
+		setLastName(lastname);
+		setSize(size);
+		setGender(gender);
+	}
 
+	public Person() {
+		this.firstname = "default";
+		this.lastname = "default";
+		this.size = 150;
+		this.gender = gender.MALE;
+	}
 
-    public Person() {
-        // TODO Auto-generated constructor stub
-    }
+	public void setFirstName(String firstname) {
+		this.firstname = HelperClass.isStringEmpty(firstname);
+		
+	}
 
-    public Person(String string, String string2, int i, Gender male) {
-        // TODO Auto-generated constructor stub
-    }
+	public void setLastName(String lastname) {
+		this.lastname = HelperClass.isStringEmpty(lastname);
+	}
 
-    public String getFirstName() {
-        return firstName;
-    }
+	public long getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
+	public void setSize(int size) {
+		this.size = HelperClass.isIntStricltyPositive(size);
+		
+		
+	}
 
-    public int getSize() {
-        return size;
-    }
+	public void setGender(Gender gender) {
+		this.gender = HelperClass.isEnumNotEmpty(gender);
+		
+	}
 
-    public Gender getGender() {
-        return gender;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((firstname == null) ? 0 : firstname.hashCode());
+		result = prime * result + ((gender == null) ? 0 : gender.hashCode());
+		result = prime * result + ((lastname == null) ? 0 : lastname.hashCode());
+		result = prime * result + size;
+		return result;
+	}
 
-    public void setFirstName(String string) {
-        // TODO Auto-generated method stub
-
-    }
-
-    public void setLastName(String string) {
-        // TODO Auto-generated method stub
-
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setSize(int i) {
-        // TODO Auto-generated method stub
-
-    }
-
-    public void setGender(Gender female) {
-        // TODO Auto-generated method stub
-
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (firstname == null) {
+			if (other.firstname != null)
+				return false;
+		} else if (!firstname.equals(other.firstname))
+			return false;
+		if (gender != other.gender)
+			return false;
+		if (lastname == null) {
+			if (other.lastname != null)
+				return false;
+		} else if (!lastname.equals(other.lastname))
+			return false;
+		if (size != other.size)
+			return false;
+		return true;
+	}
+	
+	
 
 }
