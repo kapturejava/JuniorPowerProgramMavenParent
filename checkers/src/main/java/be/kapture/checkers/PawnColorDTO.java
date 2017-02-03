db1dbd3557294c769e8911ed995501296b89b22a
Thomas Peeters <thomas.peeters@e2partners.be>
2017-02-03 15:09:01 +0100

    PawnColorDTO small example

---
+++ checkers/src/main/java/be/kapture/checkers/PawnColorDTO.java
@@ -0,0 +1,46 @@
+package be.kapture.checkers;
+
+import be.kapture.checkers.enums.Color;
+
+import java.util.List;
+
+/**
+ * Created by peeteth on 3/02/2017.
+ */
+public class PawnColorDTO {
+
+    private Color color;
+    private List<PawnLocation> pawnLocations;
+//    private List<PawnMoveHistory> pawnMoveHistoryList;
+
+    private PawnColorDTO(){} //private default constructor
+
+    /**
+     * Stel!  We willen ook een List<PawnMoveHistory> aan deze kleur associëren:
+     * Ofwel maak je een nieuwe Map<Color,List<PawnMoveHistory>>
+     * of
+     * Je voegt aan deze DTO een List<PawnMoveHistory> class variable toe en je breidt deze constructor daarmee uit
+     * @param color
+     * @param pawnLocations
+     */
+    public PawnColorDTO(Color color, List<PawnLocation> pawnLocations/* ,List<PawnMoveHistory> historyList */) {
+        this.color = color;
+        this.pawnLocations = pawnLocations;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public List<PawnLocation> getPawnLocations() {
+        return pawnLocations;
+    }
+
+    public void setPawnLocations(List<PawnLocation> pawnLocations) {
+        this.pawnLocations = pawnLocations;
+    }
+}
