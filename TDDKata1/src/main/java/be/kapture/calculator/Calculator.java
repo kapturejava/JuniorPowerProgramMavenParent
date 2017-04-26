2c5c542504a87fe0926870f7f4cd2833217332f3
vanmoj1 <jo.vanmontfort@e2partners.be>
2017-04-26 10:40:52 +0200

    Added tests for Default delimiter

--- TDDKata1/src/main/java/be/kapture/calculator/Calculator.java
+++ TDDKata1/src/main/java/be/kapture/calculator/Calculator.java
@@ -1,21 +1,45 @@
 package be.kapture.calculator;
 
+import exceptions.NegativeNumberNotAllowedException;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by vanmoj1 on 14/04/2017.
  */
 public class Calculator {
 
     int Add(String numbers) {
+        String[] intNumbers;
+        String delimiter = null;
         int sum = 0;
-        if (numbers.isEmpty()) {
-            sum = 0;
-        } else if (numbers.length() == 1) {
-            return Integer.parseInt(numbers);
-        } else {
-            Integer int1 = Integer.parseInt(numbers.substring(0, 1));
-            Integer int2 = Integer.parseInt(numbers.substring(2, 3));
 
-            sum = int1 + int2;
+        if (StringUtils.isNotEmpty(numbers)) {
+            Pattern pattern = Pattern.compile("^//(.*?)\\n");
+            Matcher matcher = pattern.matcher(numbers);
+            if (numbers.contains("-")) {
+                throw new NegativeNumberNotAllowedException();
+            }
+            if (matcher.find()) {
+                delimiter = matcher.group(1);
+            }
+            if (!(numbers.contains(",") || numbers.contains("\n"))) {
+                return Integer.parseInt(numbers);
+            } else {
+                if (StringUtils.isEmpty(delimiter)) {
+                    intNumbers = numbers.split("[\\r\\n]|,");
+                } else {
+                    numbers = StringUtils.substringAfter(numbers, "\n");
+                    intNumbers = numbers.split(delimiter);
+                }
+                for (String number : intNumbers) {
+                    sum += Integer.parseInt(number);
+                }
+            }
+        } else {
+            return sum;
         }
 
         return sum;
---
+++ TDDKata1/src/test/java/be/kapture/calculator/CalculatorTest.java
@@ -0,0 +1,67 @@
+package be.kapture.calculator;
+
+import exceptions.NegativeNumberNotAllowedException;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by vanmoj1 on 14/04/2017.
+ */
+public class CalculatorTest {
+    private Calculator calculator;
+
+    @Before
+    public void beforeMethod() {
+        calculator = new Calculator();
+    }
+
+    @Test(expected = NegativeNumberNotAllowedException.class)
+    public void testIfNegativeStringNumberThrowsException() {
+        int sum = calculator.Add("-15,10");
+
+    }
+
+    @Test
+    public void testIfTwoStringNumbersAddWithDefaultDelimiter() {
+        int sum = calculator.Add("//;\n10;20");
+        assertEquals(30, sum);
+    }
+
+    @Test
+    public void testIfUnknownNumberOfStringNumbersWithNewLineDelimiterAddCorrect() {
+        int sum = calculator.Add("15\n10,4,8\n9,4,12");
+        assertEquals(62, sum);
+    }
+
+    @Test
+    public void testIfUnknownNumberOfStringNumbersAddCorrect() {
+        int sum = calculator.Add("15,10,4,8,9,4,12");
+        assertEquals(62, sum);
+    }
+
+    @Test
+    public void testIfTwoStringNumbersWithNewLineAddCorrect() {
+        int sum = calculator.Add("15\n10");
+        assertEquals(25, sum);
+    }
+
+    @Test
+    public void testIfTwoStringNumbersAddCorrect() {
+        int sum = calculator.Add("15,10");
+        assertEquals(25, sum);
+    }
+
+    @Test
+    public void testIfOneStringNumberReturnsNumber() {
+        int sum = calculator.Add("33");
+        assertEquals(33, sum);
+    }
+
+    @Test
+    public void testIfEmptyStringNumbersReturnsZero() {
+        int sum = calculator.Add("");
+        assertEquals(0, sum);
+    }
+}
