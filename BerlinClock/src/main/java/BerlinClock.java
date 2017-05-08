8302836353771862df5d3f6c4101c0ae3c7c6111
vanmoj1 <jo.van.montfort1@telenet.be>
2017-05-08 09:29:47 +0200

    Added private method getOnOffBottomRow @BerlinCLock

--- BerlinClock/src/main/java/BerlinClock.java
+++ BerlinClock/src/main/java/BerlinClock.java
@@ -6,33 +6,31 @@ public class BerlinClock {
     }
 
     public String getBottomHours(int i) {
-        i = i % 10;
+        return getOnOffBottomRow(i);
+    }
 
-        if (i == 1) {
-            return "ROOO";
-        } else if (i == 2) {
-            return "RROO";
-        } else if (i == 3) {
-            return "RRRO";
-        } else if (i == 4) {
-            return "RRRR";
+    private String getOnOffBottomRow(int i) {
+        StringBuffer hours = new StringBuffer("OOOO");
+        int begin = 0;
+        int end = i % 5;
+
+        for (int z = 0; z < end; z++) {
+            hours.replace(z, z + 1, "R");
         }
-        return "OOOO";
+
+        return hours.toString();
     }
 
     private String getOnOffTopRow(int i) {
-        if (i <= 10) {
-            if (i == 0) {
-                return "OOOO";
-            }
-            return "ROOO";
-        } else if (10 < i && i <= 15) {
-            return "RROO";
-        } else if (15 < i && i <= 20) {
-            return "RRRO";
-        } else {
-            return "RRRR";
+        StringBuffer hours = new StringBuffer("OOOO");
+        int begin = 0;
+        int end = i / 5;
+
+        for (int z = 0; z < end; z++) {
+            hours.replace(z, z + 1, "R");
         }
+
+        return hours.toString();
     }
 
     public String getSeconds(int number) {
